31e5ec7d4381fda23c701259c27a86d02a21a0ee
marcellmueller <mlmueller@protonmail.com>
2021-02-04 14:22:34 -0800

    changed object pool capacity

--- src/game/object_list_processor.h
+++ src/game/object_list_processor.h
@@ -23,7 +23,12 @@
 /**
  * The maximum number of objects that can be loaded at once.
  */
-#define OBJECT_POOL_CAPACITY 240
+
+ //vitadrawdistance 
+// #define OBJECT_POOL_CAPACITY 240
+// trying to raise this slowly and incrementally
+
+#define OBJECT_POOL_CAPACITY 300
 
 /**
  * Every object is categorized into an object list, which controls the order
